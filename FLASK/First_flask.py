52338c4e3865477c0cec8f95ff3b3736d7e262b2
Swapnil Thomare <swapnil2001thomare@gmail.com>
2023-03-29 21:01:18 +0530

    frist flask app

---
+++ FLASK/First_flask.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from flask import Flask
+
+app =  Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello,World</h1>"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
+ 
